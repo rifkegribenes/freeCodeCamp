c96ec1fb53a1ac0aaf1fc1cf7e1bdc8293bcc9d7
Sarah Schneider <rifkegribenes@gmail.com>
2017-03-26 16:28:06 -0700

    working, added all/online/offline buttons, searchform, remove stream, and nav tabs. need to clean up code and add styling

--- twitch-tv-viewer/script.js
+++ twitch-tv-viewer/script.js
@@ -1,15 +1,26 @@
 /*jshint esversion: 6 */
-/*jshint loopfunc: true */
 $(document).ready(function() {
 
-  var users = ["comster404", "ESL_SC2", "brunofin", "twitchpresents"];
-  var logo, status, name, url, color, activity, z, data;
+ var users = ["comster404", "ESL_SC2", "brunofin", "twitchpresents"];
+ var removed = [];
+ var logo, status, name, url, color, activity, z, data;
 
-  function display(logo, status, name, url, color) {
-    $("#user-info").prepend('<div class="card trow ' + color + '" id=' + name + '> <img class="sm" src=' + logo + ' >  <div class="content"> <a href=' + url + ' target="_blank"> <h3 class="header">' + name + '</h3> </a><i class="fa fa-circle" aria-hidden="true"></i><span class="status">' + status + '</span><button class="close-stream"><i class="fa fa-times xx"></i></button></div>  </div>');
-  }
+ function display(logo, status, name, url, color) {
+    $("#user-info").prepend('<div class="card trow ' + color + '" id=' + name + '> <img class="sm" src=' + logo + ' >  <div class="content"> <a href=' + url + ' target="_blank"> <h3 class="header">' + name + '</h3> </a><i class="fa fa-circle" aria-hidden="true"></i><span class="status">' + status + '</span><button type="button" class="close-x ' + name + '" aria-label="Close"><i class="fa fa-times xx" aria-hidden="true"></i></button></div>  </div>');
 
-  function setVars(data, dstream, name, activity) {
+  $('.close-x').click(function() {
+    $(this).parents('.card').slideUp();
+    var remName = ($(this).attr('class').split(' ').pop());
+    if (removed.indexOf(remName) === -1) {
+    removed.push(remName);
+    }
+    if (users.indexOf(remName) > -1) {
+    users.splice(users.indexOf(remName), 1);
+}
+}); //remove
+  } //display
+
+ function setVars(data, dstream, name, activity) {
     if (data.logo) {
               logo = data.logo;
             } else
@@ -26,18 +37,9 @@ $(document).ready(function() {
     logo = logo;
     url = data._links.channel;
     display(logo, status, name, url, color);
-  } // setVars
+  } //setVars
 
-  var jqxhr = $.getJSON('https://wind-bow.gomix.me/twitch-api/users/freecodecamp/follows/channels?callback=?', function(data) {
-    for (let i = 0; i < data.follows.length; i++) {
-      var displayName = data.follows[i].channel.display_name;
-      users.unshift(displayName);
-    }
-  })
-
-  .done(function() {
-      //  console.log(users);
-      users.forEach(function(name) {
+ function getData(name) {
         $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + name + '?callback=?', function(data) {
           if (data.error) {
             logo = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1018136/GenericProfile.png';
@@ -52,15 +54,85 @@ $(document).ready(function() {
               var activity = dstream.stream;
               setVars(data, dstream, name, activity);
             }); // getJSON
-
           }
         }); // getJSON
-      }); // forEach
-    }) // done
+    } //getData
+
+  $('.add').click(function() {
+    name = $('.search').val();
+    getData(name);
+    users.unshift(name);
+    $(".search").val("");
+  });
+
+   $('.all').click(function() {
+      console.log("all: " + users);
+      console.log("removed: " + removed);
+    users.forEach(function(name) {
+      $('#'+name).show();
+    $('li').removeClass('active');
+    $('li').attr("aria-expanded","false");
+    $('li.all').addClass('active');
+    $('.all').attr("aria-expanded","true");
+        }); // forEach
+  });
+
+    $('.on').click(function() {
+      console.log("on: " + users);
+      console.log("removed: " + removed);
+    users.forEach(function(name) {
+      if (removed.indexOf(name) > -1) {
+      $('#'+name).show();
+    }
+      if ($('#'+name).hasClass('online')) {
+      $('#'+name).show();
+    } else {
+      $('#'+name).hide();
+    }
+    $('.notfound').hide();
+    $('li').removeClass('active');
+    $('li').attr("aria-expanded","false");
+    $('li.on').addClass('active');
+    $('.on').attr("aria-expanded","true");
+    }); //forEach
+  });
+
+    $('.off').click(function() {
+      console.log("off: " + users);
+      console.log("removed: " + removed);
+    users.forEach(function(name) {
+      if (removed.indexOf(name) > -1) {
+      $('#'+name).show();
+    }
+      if ($('#'+name).hasClass('offline')) {
+      $('#'+name).show();
+    } else {
+      $('#'+name).hide();
+    }
+    $('.notfound').hide();
+    $('li').removeClass('active');
+    $('li').attr("aria-expanded","false");
+    $('li.off').addClass('active');
+    $('.off').attr("aria-expanded","true");
+    }); //forEach
+  });
+
+ var jqxhr = $.getJSON('https://wind-bow.gomix.me/twitch-api/users/freecodecamp/follows/channels?callback=?', function(data) {
+    for (let i = 0; i < data.follows.length; i++) {
+      var displayName = data.follows[i].channel.display_name;
+      users.unshift(displayName);
+    }
+  })
+
+  .done(function() {
+      users.forEach(function(name) {
+        getData(name);
+        }); // forEach
+    }) //done
 
   .fail(function() {
       console.log("error");
     })
-    .always(function() {});
+  .always(function() {});
 
 }); //doc ready
